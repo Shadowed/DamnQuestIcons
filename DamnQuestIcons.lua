7bebcd8d9ab075fc6deee57361c651f23960330c
Shadowed <shadowed.wow@gmail.com>
2007-11-28 09:46:00 +0000

    * Switched methods of doing text checks to just strip out everything we don't like, if the check fails because a mod does something special you're out of luck

--- DamnQuestIcons.lua
+++ DamnQuestIcons.lua
@@ -1,73 +1,30 @@
--- Basically this makes sure we can go off the original text so other mods don't affect it
-local buttonIndex = 1
-local function setButtonText(...)
-	for i=1, select("#", ...), 3 do
-		getglobal("GossipTitleButton" .. buttonIndex).originalText = select(i, ...)
-		buttonIndex = buttonIndex + 1
+-- Tries to deal with incompatabilities that other mods cause
+local function stripStupid(text)
+	if( not text ) then
+		return nil
+
 	end
 	
-	if( buttonIndex > 1 ) then
-		buttonIndex = buttonIndex + 1
-	end
-end
 
-local function setButtonOptionText(...)
-	for i=1, select("#", ...), 2 do
-		getglobal("GossipTitleButton" .. buttonIndex).originalText = select(i, ...)
-		buttonIndex = buttonIndex + 1
-	end
+	-- Strip [<level crap>] <quest title>
+	text = string.gsub(text, "%[(.+)%]", "")
+	-- Strip color codes
+	text = string.gsub(text, "|cff000000(.+)|r", "%1")
+	-- Strip (low level) at the end of a quest
+	text = string.gsub(text, "(.+) %((.+)%)", "%1")
 	
-	if( buttonIndex > 1 ) then
-		buttonIndex = buttonIndex + 1
-	end
+	text = string.trim(text)
+	text = string.lower(text)
+	return text
 end
-
-hooksecurefunc("GossipFrameAvailableQuestsUpdate", setButtonText)
-hooksecurefunc("GossipFrameActiveQuestsUpdate", setButtonText)
-hooksecurefunc("GossipFrameOptionsUpdate", setButtonOptionText)
-hooksecurefunc("GossipFrameUpdate", function()
-	buttonIndex = 1
-end)
 
 function checkQuestText(buttonText, texture)
-	buttonText = string.trim(buttonText)
-	
-	local textHasBracket = string.match(buttonText, "%[")
-	
-	if( not textHasBracket ) then
-		buttonText = string.gsub(buttonText, "%-", "%%-")
-		buttonText = string.gsub(buttonText, "%(", "%%(")
-		buttonText = string.gsub(buttonText, "%)", "%%)")
-	end
+	buttonText = stripStupid(buttonText)
 	
 	for i=1, GetNumQuestLogEntries() do
 		local questName, _, _, _, _, _, isComplete = GetQuestLogTitle(i)
-		local questHasBracket = string.match(questName, "%[")
-
-		if( not questHasBracket ) then
-			questName = string.gsub(questName, "%-", "%%-")
-			questName = string.gsub(questName, "%(", "%%(")
-			questName = string.gsub(questName, "%)", "%%)")
-		end
 		
-		local isMatch
-		
-		-- Button: FooBar / Quest: FooBar
-		-- Button: [50] FooBar / Quest: [50] FooBar
-		if( textHasBracket == questHasBracket and buttonText == questName ) then
-			isMatch = true
-		
-		-- Button: FooBar / Quest: [50] FooBar
-		elseif( not textHasBracket and questHasBracket and string.match(questName, buttonText) ) then
-			isMatch = true
-		
-		-- Button: [50] FooBar / Quest: FooBar
-		elseif( textHasBracket and not questHasBracket and string.match(buttonText, questName) ) then
-			isMatch = true
-		end
-		
-		-- Matched, break and change texture
-		if( isMatch ) then
+		if( buttonText == stripStupid(questName) ) then
 			if( ( isComplete and isComplete > 0 ) or GetNumQuestLeaderBoards(i) == 0 ) then
 				SetDesaturation(texture, nil)
 				return
@@ -86,10 +43,9 @@ local function updateGossipIcons()
 	
 	for i=1, GossipFrame.buttonIndex do
 		local button = getglobal("GossipTitleButton" .. i)
-					
 		if( button:IsVisible() ) then
 			if( button.type == "Active" ) then
-				checkQuestText(button.originalText, getglobal(button:GetName() .. "GossipIcon"))
+				checkQuestText(button:GetText(), getglobal(button:GetName() .. "GossipIcon"))
 			else
 				SetDesaturation(getglobal(button:GetName() .. "GossipIcon"), nil)
 			end
@@ -104,10 +60,9 @@ local function updateQuestIcons()
 	
 	for i=1, (GetNumActiveQuests() + GetNumAvailableQuests()) do
 		local button = getglobal("QuestTitleButton" .. i)
-		
 		if( button:IsVisible() ) then
 			if( button.isActive == 1 ) then
-				checkQuestText(button.originalText, (button:GetRegions()))
+				checkQuestText(button:GetText(), (button:GetRegions()))
 			else
 				SetDesaturation((button:GetRegions()), nil)
 			end
